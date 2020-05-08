eeaa024c377443c962496c27ce7f793d3459820a
Thomas Bockhorn <thomasbockhorn1863@gmail.com>
2020-05-07 22:45:24 -0500

    Added image and title customizations

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-<div id="title_div" class="container-fluid  p-0">
-    <img class="image" src="{{ asset('images/cda-interview-guide.jpg') }}" alt="cda interview guide">
-    <div>
-        <h1 id="title" class="text-center text-white">CDA Interview Guide</h1>
-        <hr class="mx-auto" id="underline">
+<div id="title_div" class="container-fluid m-0 p-0">
+    <img id="image" class="img-fluid" src="{{ asset('images/cda-interview-guide.jpg') }}" alt="cda interview guide">
+    <div class="text-center">
+        <h1 id="title" class="text-white">CDA Interview Guide</h1>
+        <hr class="text-center" id="underline">
     </div>
 </div>
 
