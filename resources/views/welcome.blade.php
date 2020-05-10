0ea414ba3a0645888d4654bd17e8e723ac9f301b
Thomas Bockhorn <thomasbockhorn1863@gmail.com>
2020-05-09 23:28:31 -0500

    centered title and added underline

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,7 +2,10 @@
 
 @section('content')
 <div id="title_banner" class="container-fluid m-0 p-0">
-    <h1 id="title" class="text-white text-center">CDA Interview Guide</h1>
+    <div class="text-center">
+        <h1 id="title" class="text-white text-center">CDA Interview Guide</h1>
+        <hr id="title" class="title_line">
+    </div>
 </div>
 
 
